b27942aa964667b06ba3837d2c092c64b5ac6cb1
Ermiyas-T <jermiket@gmail.com>
2026-03-14 00:25:27 +0300

    added maintenance and review models and also created index.ts (central model exporter)

---
+++ server/src/models/index.ts
@@ -0,0 +1,7 @@
+export * from "./Company.js";
+export * from "./Maintenance.js";
+export * from "./Review.js";
+export * from "./Role.js";
+export * from "./User.js";
+export * from "./Verification.js";
+export * from "./Vehicle.js";
